d93f2fbd0ab500ca7ca000542704f5f6b6788163
San Dy <sandy@Sans-MacBook-Air.local>
2023-06-08 19:54:18 +0545

    first commit

---
+++ SIcalculator.js
@@ -0,0 +1,29 @@
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
+
+function compute() {
+  var principal = parseInt(document.getElementById("principal").value);
+  var rate = parseFloat(document.getElementById("rate").value);
+  var years = parseInt(document.getElementById("time").value);
+
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    var interest = principal * years * rate / 100;
+    var amount = principal + interest;
+    var result = document.getElementById("result");
+    var currentYear = new Date().getFullYear();
+    var futureYear = currentYear + years;
+
+    result.innerHTML = "If you deposit <mark>" + principal + "</mark>,<br>at an interest rate of <mark>" + rate + "%</mark>.<br>You will receive an amount of <mark>" + amount + "</mark>,<br>in the year <mark>" + futureYear + "</mark>.";
+
+  }
+}
+
+function showDate() {
+  var currentDate = new Date().toLocaleDateString();
+  document.getElementById("currentdate").innerText = currentDate;
+}
